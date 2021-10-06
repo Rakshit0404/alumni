8cf67238c4efafa0b4fa10226a8b2586b432e8cb
rajeev503 <rajeev.sahu85@gmail.com>
2021-10-06 19:18:32 +0530

    chnages in package

--- routes/userpro.js
+++ routes/userpro.js
@@ -7,7 +7,7 @@ const multer  = require('multer')
 const {storage}=require('../cloudinary_config')
 const { cloudinary } = require("../cloudinary_config");
 const upload = multer({ storage })
-
+const Cloudinary = require('cloudinary').v2;
 
 
 router.get('/userprofile',isLoggedIn,(req,res)=>{
@@ -16,10 +16,10 @@ router.get('/userprofile',isLoggedIn,(req,res)=>{
 
 
 
-router.post('/userprofile',  upload.array('i') , validateuser , catchAsync(async(req,res)=>{
+router.post('/userprofile',  upload.array('image') , validateuser , catchAsync(async(req,res)=>{
     console.log(req.body,req.files);
     const userpro=new Userpro(req.body.user);
-    userpro.image=req.files.map(f=>({url:f.path , filename:f.filename}))
+    userpro.images=req.files.map(f=>({url:f.path , filename:f.filename}))
     // console.log(images)
     console.log(userpro);
     await userpro.save();
@@ -45,30 +45,26 @@ router.get('/editprofile/:email_p',isLoggedIn,async(req,res)=>{
     res.render('userpf/editpro',{user});
 })
 
-router.put('/editprofile/:email_p',upload.array('i'),async(req,res)=>{
+router.put('/editprofile/:email_p',upload.array('image'),async(req,res)=>{
     // console.log(req.body);
     const {email_p}=req.params;
     const User=await Userpro.findOne({email: email_p });
     const id=User._id;
     const user=await Userpro.findByIdAndUpdate(id,{...req.body.user});
     const img=req.files.map(f => ({url:f.path , filename:f.filename}));
-    user.image.push(...img);
+    user.images.push(...img);
     await user.save();
-
-    if(req.body.deleteImages){
-      
-        // console.log(req.body.deleteImages[0]);
-        for(let filename of req.body.deleteImages){
-        await cloudinary.uploader.destroy(filename);
-        }
-        console.log("hello deleting images url from database")
-        await Userpro.updateOne({$pull: {image:{filename: {$in:req.body.deleteImages}}}})
+    console.log(req.body.deleteImages);
+    if (req.body.deleteImages) {
        
+        for (let filename of req.body.deleteImages) {
+            console.log(filename);
+            // await cloudinary.uploader.destroy(filename);
+            Cloudinary.uploader.destroy(filename);
+        }
+        await user.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+        console.log(user);
     }
-    
-    
-
-
     res.send('hooray');
 })
 
